d789e5b48860174435ef9da53716381d937a1a1a
robbie <robertoc.jasso.lira@gmail.com>
2026-05-09 21:09:02 -0600

    Quinto intento vistaControlador

---
+++ MVC5/src/main/java/unam/fesa/ico/controlador/controladorPrincipal.java
@@ -0,0 +1,51 @@
+package unam.fesa.ico.controlador;
+
+import unam.fesa.ico.vista.ventanaPrincipal;
+
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
+
+public class controladorPrincipal implements MouseListener {
+
+
+    private ventanaPrincipal view;
+
+    public controladorPrincipal(ventanaPrincipal vista) {
+        this.view = vista;
+        this.view.getBtnSaludar().addMouseListener(this);
+        this.view.getLblNombre().addMouseListener(this);
+    }
+
+    @Override
+    public void mouseClicked(MouseEvent e) {
+
+        if (e.getSource() == this.view.getBtnSaludar()) {
+            this.view.getLblSalida().setText("Hola avatar ; " + this.view.getTxtNombre().getText());
+        }
+
+        if (e.getSource() == this.view.getLblNombre()) {
+            System.out.println("Hola desde etiqueta nombre");
+        }
+
+    }
+
+    @Override
+    public void mousePressed(MouseEvent e) {
+
+    }
+
+    @Override
+    public void mouseReleased(MouseEvent e) {
+
+    }
+
+    @Override
+    public void mouseEntered(MouseEvent e) {
+
+    }
+
+    @Override
+    public void mouseExited(MouseEvent e) {
+
+    }
+}
